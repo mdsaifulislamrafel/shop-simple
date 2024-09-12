cbac5196717f95a54df241c404b917425a92a004
mdsaifulislamrafel <mdsaifulisalmrafel@gmail.com>
2024-09-13 02:36:31 +0600

    product section complete

--- src/component/Home/Home/Home.jsx
+++ src/component/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../Banner/Banner";
+import Product from "../Product/Product";
 
 const Home = () => {
     return (
         <div>
             <Banner />
+            <Product />
         </div>
     );
 };
---
+++ src/component/Home/Product/Product.jsx
@@ -0,0 +1,130 @@
+const Product = () => {
+    return (
+        <div className="">
+            <section className="text-gray-600 bg-zinc-400 body-font">
+
+                <div className="container px-5 py-24 mx-auto">
+                    <h2 className="bg-yellow-300 font-semibold text-2xl text-center p-4 rounded-md mb-10">টেকসই , ফ্যাশনেবল এবং আরামদায়ক সব পরিবেশে ,সব পোশাকে মানানসই</h2>
+                    <div className="grid gap-6 items-center justify-center md:grid-cols-2 lg:grid-cols-4">
+                        <div className="p-4 w-fit bg-indigo-400 text-black transform hover:scale-105 transition-transform duration-300">
+                            <a className="block relative h-48 rounded overflow-hidden">
+                                <img
+                                    alt="ecommerce"
+                                    className="object-cover object-center w-full h-full block"
+                                    src="https://images.pexels.com/photos/36351/the-eleventh-hour-disaster-alarm-clock-clock.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                                />
+                            </a>
+                            <div className="mt-4">
+                                <h3 className="text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                                <h2 className=" title-font text-lg font-medium">The Catalyzer</h2>
+                                <p className="mt-1">$16.00</p>
+                            </div>
+                        </div>
+                        <div className="p-4 w-fit bg-indigo-400 text-black transform hover:scale-105 transition-transform duration-300">
+                            <a className="block relative h-48 rounded overflow-hidden">
+                                <img
+                                    alt="ecommerce"
+                                    className="object-cover object-center w-full h-full block"
+                                    src="https://images.pexels.com/photos/36351/the-eleventh-hour-disaster-alarm-clock-clock.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                                />
+                            </a>
+                            <div className="mt-4">
+                                <h3 className="text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                                <h2 className=" title-font text-lg font-medium">The Catalyzer</h2>
+                                <p className="mt-1">$16.00</p>
+                            </div>
+                        </div>
+                        <div className="p-4 w-fit bg-indigo-400 text-black transform hover:scale-105 transition-transform duration-300">
+                            <a className="block relative h-48 rounded overflow-hidden">
+                                <img
+                                    alt="ecommerce"
+                                    className="object-cover object-center w-full h-full block"
+                                    src="https://images.pexels.com/photos/36351/the-eleventh-hour-disaster-alarm-clock-clock.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                                />
+                            </a>
+                            <div className="mt-4">
+                                <h3 className="text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                                <h2 className=" title-font text-lg font-medium">The Catalyzer</h2>
+                                <p className="mt-1">$16.00</p>
+                            </div>
+                        </div>
+                        <div className="p-4 w-fit bg-indigo-400 text-black transform hover:scale-105 transition-transform duration-300">
+                            <a className="block relative h-48 rounded overflow-hidden">
+                                <img
+                                    alt="ecommerce"
+                                    className="object-cover object-center w-full h-full block"
+                                    src="https://images.pexels.com/photos/36351/the-eleventh-hour-disaster-alarm-clock-clock.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                                />
+                            </a>
+                            <div className="mt-4">
+                                <h3 className="text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                                <h2 className=" title-font text-lg font-medium">The Catalyzer</h2>
+                                <p className="mt-1">$16.00</p>
+                            </div>
+                        </div>
+                        <div className="p-4 w-fit bg-indigo-400 text-black transform hover:scale-105 transition-transform duration-300">
+                            <a className="block relative h-48 rounded overflow-hidden">
+                                <img
+                                    alt="ecommerce"
+                                    className="object-cover object-center w-full h-full block"
+                                    src="https://images.pexels.com/photos/36351/the-eleventh-hour-disaster-alarm-clock-clock.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                                />
+                            </a>
+                            <div className="mt-4">
+                                <h3 className="text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                                <h2 className=" title-font text-lg font-medium">The Catalyzer</h2>
+                                <p className="mt-1">$16.00</p>
+                            </div>
+                        </div>
+                        <div className="p-4 w-fit bg-indigo-400 text-black transform hover:scale-105 transition-transform duration-300">
+                            <a className="block relative h-48 rounded overflow-hidden">
+                                <img
+                                    alt="ecommerce"
+                                    className="object-cover object-center w-full h-full block"
+                                    src="https://images.pexels.com/photos/36351/the-eleventh-hour-disaster-alarm-clock-clock.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                                />
+                            </a>
+                            <div className="mt-4">
+                                <h3 className="text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                                <h2 className=" title-font text-lg font-medium">The Catalyzer</h2>
+                                <p className="mt-1">$16.00</p>
+                            </div>
+                        </div>
+                        <div className="p-4 w-fit bg-indigo-400 text-black transform hover:scale-105 transition-transform duration-300">
+                            <a className="block relative h-48 rounded overflow-hidden">
+                                <img
+                                    alt="ecommerce"
+                                    className="object-cover object-center w-full h-full block"
+                                    src="https://images.pexels.com/photos/36351/the-eleventh-hour-disaster-alarm-clock-clock.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                                />
+                            </a>
+                            <div className="mt-4">
+                                <h3 className="text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                                <h2 className=" title-font text-lg font-medium">The Catalyzer</h2>
+                                <p className="mt-1">$16.00</p>
+                            </div>
+                        </div>
+                        <div className="p-4 w-fit bg-indigo-400 text-black transform hover:scale-105 transition-transform duration-300">
+                            <a className="block relative h-48 rounded overflow-hidden">
+                                <img
+                                    alt="ecommerce"
+                                    className="object-cover object-center w-full h-full block"
+                                    src="https://images.pexels.com/photos/36351/the-eleventh-hour-disaster-alarm-clock-clock.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                                />
+                            </a>
+                            <div className="mt-4">
+                                <h3 className="text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                                <h2 className=" title-font text-lg font-medium">The Catalyzer</h2>
+                                <p className="mt-1">$16.00</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+
+        </div>
+    );
+};
+
+export default Product;
